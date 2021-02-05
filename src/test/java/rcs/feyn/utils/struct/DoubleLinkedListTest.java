d532f20bc3c3ad0d8d1b20d94da83da310b9fce8
Raphael Correa <raphaelcorreaesilva@gmail.com>
2021-02-05 15:31:49 -0300

    use Junit 5

--- src/test/java/rcs/feyn/utils/struct/DoubleLinkedListTest.java
+++ src/test/java/rcs/feyn/utils/struct/DoubleLinkedListTest.java
@@ -2,7 +2,7 @@ package rcs.feyn.utils.struct;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class DoubleLinkedListTest {
   
