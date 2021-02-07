78b2a774b960aefe9bea62edfb45284486ad1737
Raphael Correa <raphaelcorreaesilva@gmail.com>
2021-02-07 16:08:29 -0300

    add helpful comment

--- src/main/java/rcs/feyn/three/optics/DiffuseLightSource3d.java
+++ src/main/java/rcs/feyn/three/optics/DiffuseLightSource3d.java
@@ -7,7 +7,13 @@ import rcs.feyn.math.linalg.Vector3d;
 
 public interface DiffuseLightSource3d extends IParticle3d, Colorable {
 
+  /**
+   * Used when position and normal are world coordinates (before applying Pipeline3d.getClippedViewSpaceCoordinates)
+   */
   public double getIntensityAt(Vector3d position, Vector3d normal); 
   
+  /**
+   * Used when position and normal are view coordinates (after applying Pipeline3d.getClippedViewSpaceCoordinates)
+   */
   public double getIntensityAt(Vector3d position, Vector3d normal, Matrix44 view); 
 }
